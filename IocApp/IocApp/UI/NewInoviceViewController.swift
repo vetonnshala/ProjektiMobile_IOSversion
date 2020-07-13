26b8b397f07db8c362af338e820a7cdf3526603b
artanthaqi <artaan.thaqi@gmail.com>
2020-07-14 00:35:22 +0200

    NewInoviceView elements added, NewInvoiceViewController picker for category added

--- IocApp/IocApp/UI/NewInoviceViewController.swift
+++ IocApp/IocApp/UI/NewInoviceViewController.swift
@@ -8,10 +8,60 @@
 
 import UIKit
 
-class NewInoviceViewController: UIViewController {
+class NewInoviceViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate, UITextFieldDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryTypes.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryTypes[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedCategory = categoryTypes[row]
+        txtkategoria.text = selectedCategory
+        
+    }
+    
 
+    var selectedCategory : String?
+    
+    var categoryTypes = ["Ushqim","Karburant","Te Tjera"]
+    
+    func createPickerView()
+    {
+        let pickerView = UIPickerView()
+        pickerView.delegate = self
+        
+        txtkategoria.inputView = pickerView
+        
+    }
+    
+    func dissmissPickerView()
+    {
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title:"Done",style: .plain,target: self,action:#selector(self.dissmissKeyboard))
+        
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        txtkategoria.inputAccessoryView = toolBar
+    }
+    
+    @objc func dissmissKeyboard(){
+        view.endEditing(true)
+    }
+    
+    
+    @IBOutlet weak var txtkategoria: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        createPickerView()
+        dissmissPickerView()
 
         // Do any additional setup after loading the view.
     }
--- IocApp/IocApp/ViewController.swift
+++ IocApp/IocApp/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBAction func txtkategoria(_ sender: Any) {
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
