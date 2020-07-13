18b9cf3e242c2f6648a930c760e12bf7f5dd6ec2
artanthaqi <artaan.thaqi@gmail.com>
2020-07-13 14:53:21 +0200

    NewInvoiceViewController Added

---
+++ IocApp/IocApp/UI/FaturaViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FaturaViewController.swift
+//  IocApp
+//
+//  Created by arton on 7/13/20.
+//  Copyright © 2020 Veton Shala. All rights reserved.
+//
+
+import UIKit
+
+class FaturaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "fatura"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
