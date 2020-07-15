15ff9984f0176287db115c72118fc268340ad8b3
artanthaqi <artaan.thaqi@gmail.com>
2020-07-15 11:18:03 +0200

    my invoices and invoicedeatils small fixed

---
+++ IocApp/IocApp/InvoiceDetailsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  InvoiceDetailsViewController.swift
+//  IocApp
+//
+//  Created by arton on 7/15/20.
+//  Copyright © 2020 Veton Shala. All rights reserved.
+//
+
+import UIKit
+
+class InvoiceDetailsViewController: UIViewController {
+
+    @IBOutlet weak var txtid: UILabel!
+    
+    @IBOutlet weak var txtitle: UILabel!
+    
+    @IBOutlet weak var txttype: UILabel!
+    
+    @IBOutlet weak var txtsum: UILabel!
+    
+    @IBOutlet weak var txtdate: UILabel!
+    
+    @IBOutlet weak var txtcomment: UILabel!
+    
+    
+    var invoice:Invoice?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        txtid.text = invoice?.id
+        txtitle.text = invoice?.title
+        txttype.text = invoice?.type
+        txtsum.text = invoice?.sum
+        txtdate.text = invoice?.date
+        txtcomment.text = invoice?.comment
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
