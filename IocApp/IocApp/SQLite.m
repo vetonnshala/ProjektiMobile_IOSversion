089f125fd36b267f6995a1c6df6769dd2dacbaa2
vehbirestelica <vehbi.res@gmail.com>
2020-07-19 20:04:39 +0200

    Database connection & Alert dialog added

---
+++ IocApp/IocApp/SQLite.m
@@ -0,0 +1,9 @@
+//
+//  SQLite.m
+//  IocApp
+//
+//  Created by Vehbi Restelica on 7/19/20.
+//  Copyright © 2020 Veton Shala. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
--- IocApp/IocApp/ViewController.swift
+++ IocApp/IocApp/ViewController.swift
@@ -7,14 +7,72 @@
 //
 
 import UIKit
+import SQLite3
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
-
+    
     
     @IBOutlet weak var tableView: UITableView!
-    
     @IBOutlet weak var txtpassword: UITextField!
     @IBOutlet weak var txtemail: UITextField!
+    
+    @IBOutlet weak var fjalekalimiReg: UITextField!
+    @IBOutlet weak var kompaniReg: UITextField!
+    @IBOutlet weak var emailReg: UITextField!
+    @IBOutlet weak var perdoruesiReg: UITextField!
+   
+    @IBAction func regjistrohuBtn(_ sender: Any) {
+        let perdoruesi = perdoruesiReg.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let emaili = emailReg.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let kompaniaID = kompaniReg.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let fjalekalimi = fjalekalimiReg.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if(perdoruesi?.isEmpty)!{
+            print ("Fusha Perdoruesi eshte e zbrazet!")
+            return;
+        }
+        if(emaili?.isEmpty)!{
+            print ("Fusha Perdoruesi eshte e zbrazet!")
+            return;
+        }
+        if(kompaniaID?.isEmpty)!{
+            print ("Fusha Perdoruesi eshte e zbrazet!")
+            return;
+        }
+        if(fjalekalimi?.isEmpty)!{
+            print ("Fusha Perdoruesi eshte e zbrazet!")
+            return;
+        }
+        
+        var stmt: OpaquePointer?
+        let insertQuery = "INSERT INTO uusers (name, email, companyID, password) values (?,?,?,?)"
+        
+        if sqlite3_prepare(db, insertQuery, -1, &stmt, nil) != SQLITE_OK {
+            print("Error binding query")
+        }
+        
+        if sqlite3_bind_text(stmt, 1, perdoruesi, -1, nil) != SQLITE_OK {
+            print("Error binding perdoruesi")
+        }
+        if sqlite3_bind_text(stmt, 2, emaili, -1, nil) != SQLITE_OK {
+            print("Error binding emaili")
+        }
+        if sqlite3_bind_int(stmt, 3, (kompaniaID! as NSString).intValue) != SQLITE_OK {
+            print("Error binding kompaniaID")
+        }
+        if sqlite3_bind_text(stmt, 3, fjalekalimi, -1, nil) != SQLITE_OK {
+            print("Error binding fjalekalimin")
+        }
+        if sqlite3_step(stmt) == SQLITE_DONE{
+            print ("User added")
+            let alert = UIAlertController(title: "Alert", message: "Perdoruesi u Shtua", preferredStyle:UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title:"Mbyll",style:UIAlertActionStyle.default, handler:nil))
+            self.present(alert, animated:true, completion:nil)
+        }
+        
+    }
+    
+    var db: OpaquePointer?
     
     let elements: [String] = ["1001","1002","1003","1004","1005","1006","1007"]
     let statusi = ["Paguar","Paguar","Ne Proces","Pa Paguar","Paguar","Ne Proces","Pa Paguar"]
@@ -43,12 +101,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         super.viewDidLoad()
         dissmissKeyboardView()
         // Do any additional setup after loading the view, typically from a nib.
-
         tableView?.delegate=self
         tableView?.dataSource = self
         
-    }
+     let fileurl = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false).appendingPathComponent("users.sqlite")
+        if sqlite3_open(fileurl.path, &db) != SQLITE_OK {
+            print ("Error opening database")
+            return
+        }
+        
+        let createTableQuery = "CREATE TABLE IF NOT EXISTS uusers (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, email TEXT, companyID INTEGER,password TEXT)"
+        
+        if sqlite3_exec(db, createTableQuery, nil, nil, nil) != SQLITE_OK {
+            print ("Error creating table")
+            return
+        }
 
+        print ("Everything okay")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -60,13 +131,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     
-    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:CustomTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
         cell.porosiaID.text=elements[indexPath.row]
         cell.statusi.text=statusi[indexPath.row]
         
         
         return cell
+        }
     }
-}
-
